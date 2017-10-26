51519ffbe8ea062c2f1418030f66493a4ef5d7c5
parthmistry23 <parth.diva23@gmail.com>
2017-10-26 13:42:00 -0500

    Adding Zoo Project

---
+++ app/src/main/java/org/parthapp/android/animalsinformation/MainActivity.java
@@ -0,0 +1,78 @@
+package org.parthapp.android.animalsinformation;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parthapp.android.animalsinformation.R;
+
+public class MainActivity extends AppCompatActivity {
+    private Button visit ;
+    private MediaPlayer mp;
+
+    @Override
+    public void onPause() {
+        mp.release();
+        super.onPause();
+    }
+    @Override
+    public void onResume() {
+        mp = MediaPlayer.create(this, R.raw.forest);
+        mp.start();
+        mp.setLooping(true);
+
+        super.onResume();
+    }
+    @Override
+    public void onStop() {
+        mp.release();
+        super.onStop();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //referSecond();
+        visit = (Button)findViewById(R.id.visit);
+        visit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent visit= new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(visit);
+            }
+        });
+    }
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+}
+
+
---
+++ app/src/main/java/org/parthapp/android/animalsinformation/SecondActivity.java
@@ -0,0 +1,184 @@
+package org.parthapp.android.animalsinformation;
+
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parthapp.android.animalsinformation.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    int[] images = {R.drawable.zoo, R.drawable.cobra, R.drawable.flamingo, R.drawable.peacock, R.drawable.tiger, R.drawable.zebra};
+    int currentImage = 1;
+    int sound_click, sound_cobra, sound_flamingo, sound_peacock, sound_tiger, sound_zebra;
+    int num_sounds_loaded;
+    boolean sounds_loaded;
+    private ImageView imageToDisplay;
+    private Button prev, next, info;
+    private SoundPool sp;
+
+    @Override
+    public void onPause() {
+        sp.release();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+
+        num_sounds_loaded = 0;
+        sounds_loaded = false;
+
+        sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        sp.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                num_sounds_loaded++;
+                if (num_sounds_loaded == 1)
+                    sounds_loaded = true;
+
+            }
+        });
+        sound_click = sp.load(this, R.raw.click, 1);
+        sound_cobra = sp.load(this, R.raw.cobra, 2);
+        sound_flamingo = sp.load(this, R.raw.flamingo, 3);
+        sound_peacock = sp.load(this, R.raw.peacock, 4);
+        sound_tiger = sp.load(this, R.raw.tiger, 5);
+        sound_zebra = sp.load(this, R.raw.zebra, 6);
+
+        super.onResume();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        sp.release();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        imageToDisplay = (ImageView) findViewById(R.id.imageToDisplay);
+        prev = (Button) findViewById(R.id.prev);
+        next = (Button) findViewById(R.id.next);
+        info = (Button) findViewById(R.id.info);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sounds_loaded) {
+                    sp.play(sound_click, 1, 1, 0, 0, 1);
+                }
+
+                if (currentImage == 5) {
+                    currentImage = 1;
+                    imageToDisplay.setImageResource(images[currentImage]);
+                    return;
+                } else {
+                    currentImage = currentImage + 1;
+                    imageToDisplay.setImageResource(images[currentImage]);
+                    return;
+                }
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sounds_loaded) {
+                    sp.play(sound_click, 1, 1, 0, 0, 1);
+                }
+
+                if (currentImage == 1) {
+                    currentImage = 5;
+                    imageToDisplay.setImageResource(images[currentImage]);
+                    return;
+                } else {
+                    currentImage = currentImage - 1;
+                    imageToDisplay.setImageResource(images[currentImage]);
+                    return;
+                }
+            }
+        });
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentImage == 1) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://en.wikipedia.org/wiki/Cobra"));
+                    startActivity(intent);
+                }
+                if (currentImage == 2) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://en.wikipedia.org/wiki/Flamingo"));
+                    startActivity(intent);
+                }
+                if (currentImage == 3) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://en.wikipedia.org/wiki/Peafowl"));
+                    startActivity(intent);
+                }
+                if (currentImage == 4) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://en.wikipedia.org/wiki/Tiger"));
+                    startActivity(intent);
+                }
+                if (currentImage == 5) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://en.wikipedia.org/wiki/Zebra"));
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        imageToDisplay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                if (currentImage == 1) {
+                    sp.play(sound_cobra, 1, 1, 0, 0, 1);
+                    sp.stop(sound_cobra);
+                    Toast.makeText(SecondActivity.this, "Histling", Toast.LENGTH_LONG).show();
+
+                }
+                if (currentImage == 2) {
+                    sp.play(sound_flamingo, 1, 1, 0, 0, 1);
+                    sp.stop(sound_flamingo);
+                    Toast.makeText(SecondActivity.this, "Gabbling", Toast.LENGTH_SHORT).show();
+
+                }
+                if (currentImage == 3) {
+                    sp.play(sound_peacock, 1, 1, 0, 0, 1);
+                    sp.stop(sound_peacock);
+                    Toast.makeText(SecondActivity.this, "Screaming", Toast.LENGTH_SHORT).show();
+                }
+                if (currentImage == 4) {
+                    sp.play(sound_tiger, 1, 1, 0, 0, 1);
+                    sp.stop(sound_tiger);
+                    Toast.makeText(SecondActivity.this, "Growling", Toast.LENGTH_SHORT).show();
+                }
+                if (currentImage == 5) {
+                    sp.play(sound_zebra, 1, 1, 0, 0, 1);
+                    sp.stop(sound_zebra);
+                    Toast.makeText(SecondActivity.this, "Whinning", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+}
